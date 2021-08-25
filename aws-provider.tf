1ac8efcf4245d604f6a2bfc18773b0811e5d502f
root <yunji_jang@tmax.co.kr>
2021-08-25 06:38:55 +0000

    Wed Aug 25 2021 - Committed via Codeherent

--- aws-provider.tf
+++ aws-provider.tf
@@ -24,3 +24,46 @@ resource "aws_ami_from_instance" "ami_from_instance2" {
 }
 
 resource "aws_default_vpc" "default_vpc" {}
+
+module "vpc1" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.6.0"
+    # Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink = false
+    # Assign IPv6 address on public subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  public_subnet_assign_ipv6_address_on_creation = false
+    # The Availability Zone for the VPN Gateway
+  vpn_gateway_az = ""
+    # Assign IPv6 address on outpost subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  outpost_subnet_assign_ipv6_address_on_creation = false
+    # The ARN of the Permissions Boundary for the VPC Flow Log IAM Role
+  vpc_flow_log_permissions_boundary = ""
+    # The fields to include in the flow log record, in the order in which they should appear.
+  flow_log_log_format = ""
+    # Name of database subnet group
+  database_subnet_group_name = ""
+    # Assign IPv6 address on intra subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  intra_subnet_assign_ipv6_address_on_creation = false
+    # List of maps of egress rules to set on the default security group
+  default_security_group_egress = [  ]
+    # Assign IPv6 address on database subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  database_subnet_assign_ipv6_address_on_creation = false
+    # List of maps of ingress rules to set on the default security group
+  default_security_group_ingress = [  ]
+    # ARN of Outpost you want to create a subnet in.
+  outpost_arn = ""
+    # Assign IPv6 address on elasticache subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  elasticache_subnet_assign_ipv6_address_on_creation = false
+    # The ARN of the KMS Key to use when encrypting log data for VPC flow logs.
+  flow_log_cloudwatch_log_group_kms_key_id = ""
+    # Specifies the number of days you want to retain log events in the specified log group for VPC flow logs.
+  flow_log_cloudwatch_log_group_retention_in_days = 1
+    # Should be true to enable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink_dns_support = false
+    # Assign IPv6 address on private subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  private_subnet_assign_ipv6_address_on_creation = false
+    # AZ where Outpost is anchored.
+  outpost_az = ""
+    # Assign IPv6 address on redshift subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  redshift_subnet_assign_ipv6_address_on_creation = false
+}
--- instance.tf
+++ instance.tf
@@ -3,15 +3,14 @@ variable "key_pair" {
 }
 
 resource "aws_instance" "ubuntu-ssh-server" {
-  ami = "ami-0b9064170e32bde34"
+  ami           = "ami-0b9064170e32bde34"
   instance_type = "t2.micro"
-  subnet_id = "${module.aws-network-configs.test-subnet-a-id}"
+  subnet_id     = "${module.aws-network-configs.test-subnet-a-id}"
   vpc_security_group_ids = [
     "${module.aws-network-configs.test-sg-id}",
-    ,
   ]
   key_name = "${var.key_pair}"
-  count = 1
+  count    = 1
   tags {
     Name = "test-instance"
   }
